099b32a653ca445e20594bb229687cb0d1e659a5
John Elder <john@codemy.com>
2021-01-07 12:54:29 +0600

    write EMAILS

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -131,3 +131,9 @@ REST_FRAMEWORK = {
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'nayzabekoff@gmail.com'
+EMAIL_HOST_PASSWORD = 'emirdinaadel06011995'
+EMAIL_PORT = 587
