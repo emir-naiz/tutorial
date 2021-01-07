3aa9a58917b1857af68451420777fadf09e62f25
John Elder <john@codemy.com>
2021-01-07 12:56:18 +0600

    write class SignUp & def activate

--- tutorial/carTrade/views.py
+++ tutorial/carTrade/views.py
@@ -1,22 +1,18 @@
+from django.contrib.auth import login
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import render
-from rest_framework import viewsets
-from rest_framework import views
-from rest_framework import generics
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from rest_framework import status
 from rest_framework.response import Response
 from .serializers import *
 from .models import *
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from .tokens import account_activation_token
 
-# class ProductViews(views.APIView): # APIView - often used for regis/auth
-#
-#     def get(self, *args, **kwargs):
-#         blogs = Product.objects.all()
-#         serializer = ProductSerializer(blogs, many=True)
-#         return Response(serializer.data)
-#
-# class ProductGenerics(generics.UpdateAPIView):
-#     queryset = Product.objects.all()
-#     serializer_class = ProductSerializer
 
 class ProductViewsets(viewsets.ModelViewSet):
     queryset = Product.objects.all()
@@ -33,4 +29,47 @@ class ManufacturerViewsets(viewsets.ModelViewSet):
 class OrderViewsets(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+class SignUp(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user)
+            })
+            to_email = request.data['email']
+            email = EmailMessage(
+                mail_subject, message, to=[to_email]
+            )
+            email.send()
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
+
+
+
 
