fe106cd16545541af92b69145e6a6e9d8df885e2
John Elder <john@codemy.com>
2021-01-12 12:34:41 +0600

    add basename for OrderViewSets

--- tutorial/carTrade/urls.py
+++ tutorial/carTrade/urls.py
@@ -10,11 +10,11 @@ router = routers.DefaultRouter()
 
 router.register('products', ProductViewsets, basename='products')
 router.register('manufacturer/products', ManufacturerViewsets)
-router.register('orders', OrderViewsets)
+router.register('orders', OrderViewsets, basename='order')
 
 urlpatterns = [
                   path('', include(router.urls)),
-                  path('register/', SignUp.as_view()),
+                  path('register/', SignUp.as_view(), name='register'),
                   path('login/',csrf_exempt(LoginView.as_view())),
                   path('^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', activate, name='activate'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
