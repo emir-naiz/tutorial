faff3f276919bb9546962a7279392c77703b7fbf
John Elder <john@codemy.com>
2021-01-13 13:06:13 +0600

    add date_unvaliable for gov_secret

--- tutorial/gov_secret/views.py
+++ tutorial/gov_secret/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import *
+from datetime import date
 # Create your views here.
 
 class DocumentsViewSet(viewsets.ModelViewSet):
@@ -14,15 +15,13 @@ class DocumentsViewSet(viewsets.ModelViewSet):
                 groups.append(mark.name)
             print(groups)
             queryset = Document.objects.filter(mark__in=groups)
+            today = date.today()
+            for doc in queryset:
+                if doc.date_unvaliable < today:
+                    doc.delete()
             return queryset
         except IndexError:
             queryset = Document.objects.first()
             return queryset
 
 
-        # if group == 'private':
-        #     queryset = Document.objects.filter(mark=group)
-        #     return queryset
-        # elif group == 'lieutenant':
-        #     queryset = Document.objects.filter(mark=group)
-        #     return queryset
